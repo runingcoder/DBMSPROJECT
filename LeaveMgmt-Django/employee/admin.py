9d8d2a41704408b4c53b11e734776311bbead0d5
runingcoder <achyutkayastha5@gmail.com>
2021-08-23 08:34:29 +0545

    unneccessary folder deleted'

--- LeaveMgmt-Django/employee/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from employee.models import Role,Department,Employee
-
-
-
-admin.site.register(Role)
-admin.site.register(Department)
-
-admin.site.register(Employee)
